94a9bc9fcb7ac9fce8c3cae677a5b446ac59c7ca
Mauricio Ocando <mauricio_budokai@hotmail.com>
2018-11-02 20:57:29 -0400

    main interface
    se me borro el beta este jeje + try & catch

--- src/com/maop/consola/Main.java
+++ src/com/maop/consola/Main.java
@@ -1,10 +1,47 @@
 package com.maop.consola;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int choice = 0;
+		
+		do {
+			
+			System.out.println("1. Consola Win");
+			System.out.println("2. Consola unix");
+			System.out.println("0. Salir");
+			Scanner sc = new Scanner(System.in);
+			try {
+				choice = sc.nextInt();
+			}
+			catch (Exception e) {
+				System.out.println("Caracteres invalidos, reinicie el programa.\n");
+			}
+		
+		
+		switch (choice) {
+		case 1: 
+			WindowsSyntax winSyn = new WindowsSyntax();
+			winSyn.runWin();
+			break;
+			
+		case 2:
+			UnixSyntax unixSyn = new UnixSyntax();
+			unixSyn.runUnix();
+			break;
+			
+		case 0: System.exit(0);
+		
+		default:
+			System.out.print("Selecciona una opcion valida.\n\n");
+			break;
+		}
+		
+
+}while(true);
 
 	}
-
 }
